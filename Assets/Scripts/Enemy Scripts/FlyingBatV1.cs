14b0d9ac625ca1b699dd04e8c12e93a315f190ba
Dinis Costa <scorpaust@hotmail.com>
2021-06-24 15:54:58 +0100

    Flying Bat Movement.

---
+++ Assets/Scripts/Enemy Scripts/FlyingBatV1.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingBatV1 : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeedHorizontal = 2f;
+
+    [SerializeField]
+    private float moveSpeedVertical = -2f;
+
+    [SerializeField]
+    private float horizontalMovementTreshold = 7f, verticalMovementTreshold = 4f;
+
+    private float minX, maxX, minY, maxY;
+
+    private Vector3 tempPos;
+
+    private bool moveHorizontal = true;
+
+    private bool moveVertical;
+
+    private SpriteRenderer sr;
+
+	private void Awake()
+	{
+        minX = transform.position.x - horizontalMovementTreshold;
+
+        maxX = transform.position.x + horizontalMovementTreshold;
+
+        minY = transform.position.y - verticalMovementTreshold;
+
+        maxY = transform.position.y + verticalMovementTreshold;
+
+        moveVertical = Random.Range(0,2) > 0 ? true : false;
+
+        if (Random.Range(0,2) > 0)
+		{
+            moveSpeedHorizontal *= -1f;
+		}
+
+        sr = GetComponent<SpriteRenderer>();
+	}
+
+	private void Update()
+	{
+        HandleHorizontalMovement();
+
+        HandleVerticalMovement();
+	}
+
+    private void HandleHorizontalMovement()
+	{
+        if (moveHorizontal)
+		{
+            tempPos = transform.position;
+
+            tempPos.x += moveSpeedHorizontal * Time.deltaTime;
+
+            if (tempPos.x > maxX)
+			{
+                moveSpeedHorizontal = -Mathf.Abs(moveSpeedHorizontal);
+			}
+
+            if (tempPos.x < minX)
+			{
+                moveSpeedHorizontal = Mathf.Abs(moveSpeedHorizontal);
+			}
+
+            transform.position = tempPos;
+
+            if (moveSpeedHorizontal < 0)
+			{
+                sr.flipX = true;
+			}
+            else if (moveSpeedHorizontal > 0)
+			{
+                sr.flipX = false;
+			}
+		}
+	}
+
+    private void HandleVerticalMovement()
+    {
+        if (moveVertical)
+		{
+            tempPos = transform.position;
+
+            tempPos.y += moveSpeedVertical * Time.deltaTime;
+
+            if (tempPos.y > maxY)
+                moveSpeedVertical = -Mathf.Abs(moveSpeedVertical);
+            else if (tempPos.y < minY)
+                moveSpeedVertical = Mathf.Abs(moveSpeedVertical);
+
+            transform.position = tempPos;
+		}
+    }
+
+}
---
+++ Assets/Scripts/Enemy Scripts/FlyingBatV2.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyingBatV2 : MonoBehaviour
+{
+    [SerializeField]
+    private float minX = -8.3f, maxX = 8.3f, minY = -4.5f, maxY = 4.5f;
+
+    [SerializeField]
+    private float moveSpeed = 2f;
+
+    private Vector3 targetPos;
+
+    private SpriteRenderer sr;
+
+	private float prevXPos;
+
+	private void Awake()
+	{
+		sr = GetComponent<SpriteRenderer>();
+	}
+
+	private void Update()
+	{
+        MoveToTargetPos();
+	}
+
+    private void MoveToTargetPos()
+	{
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, targetPos) < 0.1f)
+		{
+            targetPos = new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY), 0f);
+
+			prevXPos = transform.position.x;
+		}
+
+		ChangeFacingDirection();
+	}
+
+	private void ChangeFacingDirection()
+	{
+		if (transform.position.x > prevXPos)
+			sr.flipX = false;
+		else if (transform.position.x < prevXPos)
+			sr.flipX = true;
+	}
+}
